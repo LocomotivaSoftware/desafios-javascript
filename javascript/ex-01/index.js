1522bb59daaa2735cb8dd5cc82c72458b43cb21d
Rander Oliveira <rander.oliveira@escolaemmovimento.com.br>
2021-07-23 13:23:07 -0300

    adiciona primeiro exercicio

---
+++ javascript/ex-01/index.js
@@ -0,0 +1,39 @@
+const participantes = [
+    { nome: "David", pontuacao: 80, idade: 25 },
+    { nome: "Rafael", pontuacao: 75, idade: 20 },
+    { nome: "Fulano", pontuacao: 12, idade: 65 },
+    { nome: "Beltrano", pontuacao: 65, idade: 12 },
+    { nome: "Ciclano", pontuacao: 12, idade: 24 }
+];
+
+// Deve retornar o nome do participante que obteve a maior pontuacao
+function quemGanhou() {
+
+}
+
+// Deve retornar a pontuacao média dos participantes
+function pontuacaoMedia() {
+
+}
+
+// Deve retornar a soma da pontuacao de todos os participantes
+function pontuacaoTotal() {
+
+}
+
+// Deve retornar a idade do participante mais velho
+function veterano() {
+
+}
+
+// Deve retornar o número de participantes do torneio
+function numeroDeParticipantes() {
+
+}
+
+
+console.log(`O vencedor é ${quemGanhou()}!`);
+console.log(`Tivemos nessa edição um total de ${numeroDeParticipantes()} participantes`);
+console.log(`A pontuacao média dos participantes foi de: ${pontuacaoMedia()} pontos`);
+console.log(`O pontuacao total dos participantes foi ${pontuacaoTotal()} pontos`);
+console.log(`O participante veterano dessa edição tem ${veterano()} anos`);
---
+++ javascript/ex-01/instructions.md
@@ -0,0 +1,7 @@
+## Exercício 01
+
+#### Exercício sobre objetos e tipos
+
+#### O que deve ser feito
+
+Implementar as funções do arquivo index.js de acordo com a especificação dos comentários.
