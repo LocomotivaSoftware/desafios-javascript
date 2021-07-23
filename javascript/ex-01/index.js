deb1e964f7589a95a0cbb0b8e4a101ae51890438
David Mota <83174541+davidsilvamota@users.noreply.github.com>
2021-07-23 18:48:23 -0300

    total-idade-vencedor

--- javascript/ex-01/index.js
+++ javascript/ex-01/index.js
@@ -1,39 +1,53 @@
 const participantes = [
-    { nome: "David", pontuacao: 80, idade: 25 },
-    { nome: "Rafael", pontuacao: 75, idade: 20 },
-    { nome: "Fulano", pontuacao: 12, idade: 65 },
-    { nome: "Beltrano", pontuacao: 65, idade: 12 },
-    { nome: "Ciclano", pontuacao: 12, idade: 24 }
+  { nome: "David", pontuacao: 80, idade: 25 },
+  { nome: "Rafael", pontuacao: 75, idade: 20 },
+  { nome: "Fulano", pontuacao: 12, idade: 65 },
+  { nome: "Beltrano", pontuacao: 65, idade: 12 },
+  { nome: "Ciclano", pontuacao: 12, idade: 24 }
 ];
+let total = participantes.length;
+let idade = participantes[0].idade;
+let vencedor = participantes[0].nome;
 
+for (let id in participantes) {
+  if (participantes[id].idade > idade) {
+    idade = participantes[id].idade;
+  }
+}
+for (let pt in participantes) {
+  if (participantes[pt].pontuacao > vencedor) {
+    vencedor = participantes[pt].pontuacao;
+  }
+}
 // Deve retornar o nome do participante que obteve a maior pontuacao
 function quemGanhou() {
-    return(participantes[0].nome)
+  return vencedor;
 }
 
 // Deve retornar a pontuacao média dos participantes
-function pontuacaoMedia() {
-
-}
+function pontuacaoMedia() {}
 
 // Deve retornar a soma da pontuacao de todos os participantes
-function pontuacaoTotal() {
-
-}
+function pontuacaoTotal() {}
 
 // Deve retornar a idade do participante mais velho
 function veterano() {
-    return(participantes[2].idade)
+  return idade;
 }
 
 // Deve retornar o número de participantes do torneio
-function numeroDeParticipantes(numero) {
-    return (participantes.length)
+function numeroDeParticipantes() {
+  return total;
 }
 
-
 console.log(`O vencedor é ${quemGanhou()}!`);
-console.log(`Tivemos nessa edição um total de ${numeroDeParticipantes()} participantes`);
-console.log(`A pontuacao média dos participantes foi de: ${pontuacaoMedia()} pontos`);
-console.log(`O pontuacao total dos participantes foi ${pontuacaoTotal()} pontos`);
+console.log(
+  `Tivemos nessa edição um total de ${numeroDeParticipantes()} participantes`
+);
+console.log(
+  `A pontuacao média dos participantes foi de: ${pontuacaoMedia()} pontos`
+);
+console.log(
+  `O pontuacao total dos participantes foi ${pontuacaoTotal()} pontos`
+);
 console.log(`O participante veterano dessa edição tem ${veterano()} anos`);
